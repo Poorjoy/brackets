c473a3e3808831451c0603d5c96de50e0ea6f7e1
Poorjoy <madison_shepard@mail.ru>
2020-03-16 21:40:17 +0300

    feat: add brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let result = [];
+
+    for (let bracket of str) {
+        let [startBracket, endBracket] = bracketsConfig.find(pair =>
+            pair.includes(bracket)
+        );
+        if (bracket === startBracket && !result.includes(startBracket)) {
+            result.push(endBracket);
+            continue;
+        }
+        let stackedBracket = result.pop();
+        if (bracket === stackedBracket) {
+            continue;
+        }
+        return false;
+    }
+
+    return !result.length;
+};
